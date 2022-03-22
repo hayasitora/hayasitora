f6223edbb70bd5f28232f8dadc2608d7967b98e5
ADMIN <ADMIN@DESKTOP-JSKA3LU>
2022-03-22 14:48:32 +0900

    multiplication_All

---
+++ src/today2022_03_22/multiplication_All.java
@@ -0,0 +1,26 @@
+package today2022_03_22;
+
+public class multiplication_All {
+	
+	public static void main(String[]args) {
+		
+		//get multiplication all 2~9
+		
+		int i,j= 0;
+				for(j=2; j<10; j++) {
+					System.out.println("%d단이에요".formatted(j));
+					
+					
+					for (i=1; i<10; i++) {
+						
+						if(i>0)
+						System.out.println("%d*%d=".formatted(j,i)+j*i);
+						
+					}
+					
+				}
+		
+		
+	}
+
+}
