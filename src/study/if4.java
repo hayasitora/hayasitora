72c5c8333150a34a17bea6ef0b10c8b9f1987f38
ADMIN <ADMIN@DESKTOP-JSKA3LU>
2022-03-22 14:06:25 +0900

    gugudan commit

---
+++ src/study/if4.java
@@ -0,0 +1,22 @@
+package study;
+import java.util.Scanner;
+public class if4 {
+
+	
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		int i=0;
+		System.out.println("받고싶은 구구단 을 입력해주세요");
+		int j=scan.nextInt();
+		
+		for(i = 1; i<10; i++ ) {
+			System.out.println(j*i);
+			System.out.println();
+			
+		
+		}
+		
+	}
+}
