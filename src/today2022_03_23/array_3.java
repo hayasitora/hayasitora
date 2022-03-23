8a264aab0c2c5eec1f76e612592363ccddb943fb
ADMIN <ADMIN@DESKTOP-JSKA3LU>
2022-03-23 12:38:12 +0900

    array home work

--- src/today2022_03_23/array_3.java
+++ src/today2022_03_23/array_3.java
@@ -1,5 +1,57 @@
 package today2022_03_23;
 
 public class array_3 {
+	public static void main(String[]args) {
 
+		byte[] B= new byte[5];
+		B[0]=1;
+		B[1]=2;
+		B[2]=3;
+		B[3]=4;
+		B[4]=5;
+		
+		char[] C= new char[5];
+		C[0]='A';
+		C[1]='B';
+		C[2]='C';
+		C[3]='D';
+		C[4]='E';
+		
+		boolean[] b=new boolean[5];
+		b[0]=false;
+		b[1]=true;
+		b[2]=false;
+		b[3]=true;
+		b[4]=false;
+		
+		short[] S=new short[5];
+		S[0]=100;
+		S[1]=200;
+		S[2]=300;
+		S[3]=400;
+		S[4]=500;
+		
+		long[] L=new long[5];
+		L[0]=90;
+		L[1]=135;
+		L[2]=180;
+		L[3]=225;
+		L[4]=270;
+
+		
+		float[] F=new float[5];
+		F[0]=(float) 3.14;
+		F[1]=(float) 6.28;
+		F[2]=(float) 9.42;
+		F[3]=(float) 12.56;
+		F[4]=(float) 15.70001;
+		
+		double[] D=new double[5];
+		D[0]=3.141592;
+		D[1]=3.141592;
+		D[2]=3.141592;
+		D[3]=3.141592;
+		D[4]=3.141592;
+		
+	}
 }
--- src/today2022_03_23/array_4.java
+++ src/today2022_03_23/array_4.java
@@ -3,8 +3,20 @@ package today2022_03_23;
 public class array_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//index a를 선언
+		int[]a;
+		//index a의 값을 선언
+		a=new int[5];
+		//index a의 0번째 값을 선언
+		a[0]=1;
+		//index a의 1번째 값을 선언
+		a[1]=1;
+		//index a의 2번째 값을 선언
+		a[2]=1;
+		//index a의 3번째 값을 선언
+		a[3]=1;
+		
 	}
 
 }
