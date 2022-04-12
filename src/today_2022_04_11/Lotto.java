ebcd3a76b64196067ee5df03cecdf76176e8b920
ユーズ <55609079+hayasitora@users.noreply.github.com>
2022-04-12 13:50:03 +0900

    Update Lotto.java

--- src/today_2022_04_11/Lotto.java
+++ src/today_2022_04_11/Lotto.java
@@ -16,21 +16,21 @@ class latto{
 		int []stack=new int [60];
 		int max=0;
 		int maxindex = 0;
-		for(int i=1; i<61; i++) {
+		for(int i=0; i<60; i++) {
 			num[i]=(int)(Math.random()*60)+1;
 		}
-		for(int i=1; i<61; i++) {
+		for(int i=0; i<60; i++) {
 			stack[i]=0;
 		}
-		for(int i=1; i<61; i++) {
-			for(int j=1; j<61; j++) {
+		for(int i=0; i<60; i++) {
+			for(int j=0; j<60; j++) {
 				if(num[i]==j) {
 					stack[j]+=1;
 				}
 			}
 			
 		}
-		for(int i=1;i<61; i++) {
+		for(int i=0;i<60; i++) {
 			if(stack[i]>max) {
 				max=stack[i];
 				maxindex=i;
